4d5494f1c0a1b44fe6977f600d44b5039f25c077
Sher Sheikh <sher.sheikh77@gmail.com>
2019-10-14 21:02:36 -0400

    All color functions working

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let gridDivs;
 
 
 function createGrid(numBox = 16) {
-    
+
     for (let i = 0; i < numBox; i++) {
         container.append(row.cloneNode());
     }
@@ -15,10 +15,11 @@ function createGrid(numBox = 16) {
         for (let i = 0; i < numBox; i++) {
             let div = document.createElement('div');
             div.classList.add('grid-item');
-            div.setAttribute('style',`width: ${100/numBox}%; padding-top:${100/numBox}%;
+            div.setAttribute('style', `width: ${100 / numBox}%; padding-top:${100 / numBox}%;
             float: left;
             outline: 1px solid black;
-            background-color: white;`);
+            background-color: white;
+            filter: brightness(1);`);
             // div.style.width = `'${100/numBox}'%;`;
             // div.style.paddingTop = `'${100/numBox}'%`;
             row.appendChild(div);
@@ -27,30 +28,59 @@ function createGrid(numBox = 16) {
     gridDivs = document.querySelectorAll('.grid-item');
 }
 
-function rainbowSketch(){
-gridDivs.forEach((div) => {
-    console.log(gridDivs)
-    div.addEventListener('mouseover', (e) => {
-        console.log(e.target.style);
-        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        console.log(randomColor)
-        e.target.style.background = `#${randomColor}`;
+function rainbowSketch() {
+    gridDivs.forEach((div) => {
+        div.addEventListener('mouseover', (e) => {
+            var randomR = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+            var randomG = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+            var randomB = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+            let alpha = 1;
+            e.target.style.backgroundColor = `rgba(${randomR},${randomG},${randomB},${1})`;
+        })
     })
-})
 }
-function isNumber(value){
+function blackSketch() {
+    gridDivs.forEach((div) => {
+        div.addEventListener('mouseover', (e) => {
+            e.target.style.background = `black`;
+        })
+    })
+}
+function getValueBetweenBrackets(value){
+    let betweenBracketRegex = /\(([^)]+)\)/;
+    console.log(`This is value: ${betweenBracketRegex.exec(value)}`)
+    return Number(betweenBracketRegex.exec(value)[1]);
+}
+function shaderSketch() {
+    gridDivs.forEach((div) => {
+        div.addEventListener('mouseover', (e) => {
+            if(e.target.style.backgroundColor === 'black' || e.target.style.filter === "brightness(0)"){
+                console.log('itsblack');
+                return;
+            }else{
+                let shade = e.target.style.filter;
+                let currentBrightness = getValueBetweenBrackets(shade);
+                e.target.style.filter = `brightness(${currentBrightness-0.1})`;
+            }
+        })
+    })
+}
+
+
+
+function isNumber(value) {
     let numRegex = /^[1-9][0-9]*$/;
     return numRegex.test(value);
 }
-function clearGrid(){
-    document.querySelectorAll('.grid-item').forEach(function(e){
+function clearGrid() {
+    document.querySelectorAll('.grid-item').forEach(function (e) {
         e.remove();
     });
-    document.querySelectorAll('.row').forEach(function(e){
+    document.querySelectorAll('.row').forEach(function (e) {
         e.remove();
     });
     let newGridSize = prompt('Choose the size of your grid (Example: "16" will create a 16x16 grid');
-    while(!isNumber(newGridSize) || newGridSize===null){
+    while (!isNumber(newGridSize) || newGridSize === null) {
         newGridSize = prompt('Must enter a number. (Example: "16" will create a 16x16 grid');
     }
     createGrid(newGridSize);
